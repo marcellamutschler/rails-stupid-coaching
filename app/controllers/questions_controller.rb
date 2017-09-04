33fec8e68653c47b56a8730b5b335c0cabbc84e6
Marcella Mutschler <marcella.mutschler@whu.edu>
2017-09-04 14:35:32 +0200

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,13 @@
+class QuestionsController < ApplicationController
+  def answer
+    @question = params[:query]
+    if @question.include? "?"
+      @answer = "Silly question son"
+    else
+      @answer = "I don't care my boy"
+    end
+  end
+
+  def ask
+  end
+end
